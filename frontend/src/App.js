37d9929ee58d0adb82a265ea63ef81598817b27b
Marcos Paulo <marcosp.mail@gmail.com>
2020-01-15 12:58:10 -0200

    feat: Implements the main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,95 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
 function App() {
   return (
-    <h1>aaa</h1>
+    
+    <div id="app">
+
+      <aside>
+        <strong>Cadastrar</strong>
+
+        <form>
+
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+
+      </aside>
+
+      <main>
+
+        <ul>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://i.pravatar.cc/300" alt="User image" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnolgias de desenvolvimento</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://i.pravatar.cc/300" alt="User image" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnolgias de desenvolvimento</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://i.pravatar.cc/300" alt="User image" />
+              <div className="user-info">
+                <strong>Diego Fernandes</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>CTO na @Rocketseat. Apaixonado pelas melhores tecnolgias de desenvolvimento</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+
+        </ul>
+
+      </main>
+
+    </div>
   );
 }
 
